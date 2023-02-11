0d0a4a0580c3736babebd4555986bf041955c2ed
Darshil Patel <darshilpatel1990@gmail.com>
2023-02-11 11:49:10 -0600

    Workshop Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-import './App.css';
+import CampsitesList from "./features/campsites/CampsitesList";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-        <Navbar dark color='primary' sticky='top' expand='md'>
-            <Container>
-                <NavbarBrand href='/'>
-                    <img src={NucampLogo} alt='nucamp logo' />
-                </NavbarBrand>
-            </Container>
-        </Navbar>
-        I'm ready for workshop!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <CampsitesList />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
