51d2fc2819912ae38246b4e85a409b17804e0e8e
Darshil Patel <darshilpatel1990@gmail.com>
2023-02-15 20:18:20 -0600

    Homepage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -8,6 +8,10 @@ export const selectCampsitebyId = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === id);
 };
 
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
+
 
 // export const selectRandomCampsite = () => {
 //     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
