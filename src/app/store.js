906c838271020698495e95d317a7ab71c4785f72
Darshil Patel <darshilpatel1990@gmail.com>
2023-02-28 22:37:19 -0600

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,7 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer} from '../features/promotions/promotionsSlice';
-
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
@@ -11,5 +11,6 @@ export const store = configureStore({
     comments: commentsReducer,
     partners: partnersReducer,
     promotions: promotionsReducer
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
